3465d73c134a40863ff7db7d2ea15f748e0540f6
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-10-06 20:56:37 -0700

    added a couple dependencies and expanded on starter code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import uuid from 'uuid'; // for id generation
-import bcrypt from 'bcrypt'; // for password encryption
+import { v4 as uuidv4 } from 'uuid'; // for id generation https://www.npmjs.com/package/uuid
+import bcrypt from 'bcrypt'; // for password encryption https://www.npmjs.com/package/bcrypt
 import dotenv from 'dotenv';
+import joi from 'joi'; // for validation of user input https://joi.dev/api/
 dotenv.config();
 
 import * as db from './database.js';
@@ -18,7 +19,7 @@ app.use(cookieSession({ name: 'session', keys: [process.env.SESSION_KEY] }));
 const api = express.Router();
 app.use('/api/v1', api);
 
-api.post('/login', (req, res) => {
+api.post('/login', async (req, res) => {
   const { email, password } = req.body;
   db.User.findOne({ email }, (err, user) => {
     if (err) {
@@ -45,6 +46,40 @@ api.post('/login', (req, res) => {
 api.post('/logout', (req, res) => {
   req.session = null;
   res.status(200).send('Logout successful');
+});
+
+api.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+  const hash = await bcrypt.hashSync(password, 10);
+
+  const validateUser = joi.object({
+      name: joi.string().min(3).required(),
+      email: joi.string().min(5).required().email(),
+      password: joi.string().min(5).required()
+  }).validate(user);
+
+  if (validateUser.error) {
+    res.status(400).send(validateUser.error.details[0].message);
+    return;
+  }
+
+  const user = new db.User({
+    id: uuidv4(),
+    name,
+    email,
+    password: hash,
+    date: Date.now()
+  });
+
+  try {
+    const result = await user.save();
+    req.session.user = result;
+    res.status(200).send('Signup successful');
+  } catch (err) {
+    // TODO: don't send the backend error to real users
+    res.status(500).send(err);
+  }
+
 });
 
 module.exports = app;
--- database.js
+++ database.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
-import joi from 'joi'; // for validation of user input
+import mongoose from 'mongoose'; // https://mongoosejs.com/docs/guide.html
 
 const db = mongoose.connect(
   'mongodb://localhost:27017/MyDatabase', 
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
 });
 
-// below is just an example of how we can use mongoose to create a model and joi to validate it
+// below is just an example of how we can use mongoose
 
 const userSchema = new mongoose.Schema({
   id: String,
@@ -20,14 +20,5 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
-
-const validateUser = (user) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-    email: Joi.string().min(5).required().email(),
-    password: Joi.string().min(5).required()
-  });
-  return schema.validate(user);
-}
 
 module.exports = { User, validateUser };
