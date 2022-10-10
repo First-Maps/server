c55e0ca60984e6426a2f0b2bae561620a15a8877
Kevin Hung <kevincjhung@gmail.com>
2022-10-09 17:40:56 -0700

    .

---
+++ firstmaps/stories/Form.jsx
@@ -0,0 +1,12 @@
+import "./Form.css"
+
+export default function Form(){
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="name">Name</label>
+                <input type="number" name="phoneNumber" id="phoneNumber" />
+            </form>
+        </div>
+    )
+}
